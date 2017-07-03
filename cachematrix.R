ebdab952ee0e9ad697de61b9e320700105cb81d9
danielsligte <djsligte@gmail.com>
2017-07-03 17:03:55 +0200

    Solution Programming Assignment 2
    Solution Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this script I will make a matrix object that can cache its inverse
+## Subsequently, I will write a function that actually returns the inverse of a matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## make a matrix that can cache its inverse
+makeCacheMatrix <- function(x = matrix())  {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inverse <<- inverse
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+### test of the makematrix function
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4),nrow=2,ncol=2) )
+amatrix$get()
+amatrix$getinverse()
 
-## Write a short comment describing this function
+## make the function that caches the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
+
+## and now the test if caching the inverse works:
+amatrix$getinverse()
+cacheSolve(amatrix)
+amatrix$get()
+amatrix$getinverse()
+cacheSolve(amatrix)
